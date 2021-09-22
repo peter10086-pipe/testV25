a7bcdd2bebac08297953e48dffed8b3ea9c23a87
peter.zhang <peter.zhang@ucloud.cn>
2021-09-22 10:44:34 +0800

    fumesh

--- control/controller.go
+++ control/controller.go
@@ -47,7 +47,12 @@ func FullMesh(c *gin.Context) {
 
 			//初始化远程
 
-			rpc, err := rpc.DialHTTP("tcp","127.0.0.1:8082")
+
+		switch i%3 {
+
+		case 0:
+
+			rpc, err := rpc.DialHTTP("tcp","10.2.122.25:8082")
 			if err !=nil{
 
 				c.JSON(http.StatusBadRequest, gin.H{
@@ -56,11 +61,6 @@ func FullMesh(c *gin.Context) {
 				})
 			}
 
-
-
-		switch i%3 {
-
-		case 0:
 				err1 := rpc.Call("VPC25Cube.FullMeshPing", Params{50,10,v,tempArray}, &ret)
 
 				if err1 != nil {
@@ -73,6 +73,14 @@ func FullMesh(c *gin.Context) {
 
 				fmt.Println(ret)
 		case 1:
+			rpc, err := rpc.DialHTTP("tcp","10.2.202.109:8082")
+			if err !=nil{
+
+				c.JSON(http.StatusBadRequest, gin.H{
+					"retcode": "-1",
+					"message":err.Error(),
+				})
+			}
 			fmt.Println(v)
 			fmt.Println(tempArray)
 			err1 := rpc.Call("VPC25Cube.FullMeshPing", Params{50,100,v,tempArray}, &ret)
@@ -88,6 +96,14 @@ func FullMesh(c *gin.Context) {
 
 			fmt.Println(ret)
 		case 2:
+			rpc, err := rpc.DialHTTP("tcp","10.2.7.222:8082")
+			if err !=nil{
+
+				c.JSON(http.StatusBadRequest, gin.H{
+					"retcode": "-1",
+					"message":err.Error(),
+				})
+			}
 			err1 := rpc.Call("VPC25Cube.FullMeshPing", Params{50,1000,v,tempArray}, &ret)
 
 			if err1 != nil {
